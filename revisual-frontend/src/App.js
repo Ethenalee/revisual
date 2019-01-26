10cb21d507c6cb22df412b03b4a9c0cf8748b9f9
ethenalee <yeonji900111@gmail.com>
2019-01-26 15:03:53 -0500

    added navbar, test img

--- revisual-frontend/src/App.js
+++ revisual-frontend/src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Map from './map.jsx';
+import { Navbar } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#home">REvisual</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Navbar.Link className="Register" href="#" pullRight>Register</Navbar.Link>
+            <Navbar.Text className="SignIn" pullRight>Welcom Ethena!</Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>
+        <Map />
       </div>
     );
   }
---
+++ revisual-frontend/src/map.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import MapImg from './images/map.png'
+
+class Map extends Component {
+  
+  render() {
+    return (
+        <div className="map">
+          <img className="map-img" alt='' src={MapImg} />
+        </div>
+    );
+  }
+}
+export default Map;
