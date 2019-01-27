ca70f5b1eead6c1312c242b3d6bf7dd62cc3bb43
Zixia Lu <zixialu@gmail.com>
2019-01-27 00:15:22 -0500

    Change municipality details to fetch data from backend and display it

--- revisual-frontend/src/MunicipalityDetails.jsx
+++ revisual-frontend/src/MunicipalityDetails.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 
-function MunicipalityDetails({ match }) {
-  return (
-    <div>
-      <h3>ID: {match.params.id}</h3>
-    </div>
-  );
+class MunicipalityDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      id: null,
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ id: this.props.match.params.id });
+
+    // TODO: Soft-code this url
+    fetch(`http://localhost:3001/municipalities/${this.props.match.params.id}`)
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>ID: {this.state.id}</h3>
+        <h3>Municipality: {this.state.data && this.state.data.municipality}</h3>
+        <h6>Raw Data: {JSON.stringify(this.state.data)}</h6>
+      </div>
+    );
+  }
 }
 
 export default MunicipalityDetails;
