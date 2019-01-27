4b79d1621a996810265c1abc0b79f7096145ea0c
ethenalee <yeonji900111@gmail.com>
2019-01-27 05:01:33 +0000

    add intro img and jsx file

--- revisual-frontend/src/App.js
+++ revisual-frontend/src/App.js
@@ -1,28 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Intro from './intro.jsx';
 import Map from './map.jsx';
-import { Navbar } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#home">REvisual</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <div className="SignIn-Register">
-            <Navbar.Text className="Register">
-            <Navbar.Link href="#">Register</Navbar.Link>
-            </Navbar.Text>
-            <Navbar.Text className="SignIn">Welcom Ethena!</Navbar.Text>
-            </div>
-          </Navbar.Collapse>
-        </Navbar>
+        <Intro/>
         <Map />
       </div>
     );
