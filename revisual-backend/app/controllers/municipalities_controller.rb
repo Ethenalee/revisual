a253bb70ccb12167ce6ec99b99c672e6e9d42af7
Zixia Lu <zixialu@gmail.com>
2019-01-29 13:11:51 -0500

    Implement filtering by listing type, add average sold price to api json

--- revisual-backend/app/controllers/municipalities_controller.rb
+++ revisual-backend/app/controllers/municipalities_controller.rb
@@ -7,9 +7,18 @@ class MunicipalitiesController < ApplicationController
   def show
     # Show detailed stats for a single municipality
     @municipality = Municipality.find params[:id]
+    @timeframe = params[:timeframe]
+    @is_sale = ActiveRecord::Type::Boolean.new.cast(params[:is_sale])
+    @type = params[:type]
+
+    puts("Query params:")
+    puts(@timeframe, @is_sale, @type)
 
     # TODO: Add all the stats to this json response
-    json_response(@municipality)
+    json_response({
+      municipality: @municipality,
+      average_sold_price: average_sold_price(@timeframe, @is_sale, @type)
+    })
   end
 
   private
@@ -17,19 +26,25 @@ class MunicipalitiesController < ApplicationController
   # Returns the average sale price of sold properties that match the filter.
   # TODO: Implement optional type filtering
   def average_sold_price(timeframe, is_sale, type)
-    # Decide how far back to filter sales
+    # Choose timeframe to filter sales by
     # TODO: Refactor this out into a private helper method
     filter_datetime = case timeframe
-      when "today" then DateTime.beginning_of_day()
+      when "today" then 1.day.ago.to_datetime
       when "3months" then 3.months.ago.to_datetime
       when "2years" then 2.years.ago.to_datetime
       else 5.years.ago.to_datetime
     end
 
-    Municipality.where(municipality_id: params[:id])
+    # Filter listings by required fields
+    filter = Listing.where(municipality_id: params[:id])
       .where(sale_lease: is_sale ? "sale" : "lease")
       .where("sold_date > ?", filter_datetime) # TODO: Make sure this works
-      .average(:sold_price)
+
+    # Further filter by type, if provided
+    filter = filter.where(listing_type: type) if type
+
+    # Find the average sold price of filtered listings
+    filter.average(:sold_price)
   end
 
   def number_of_listings(timeframe, is_sale, type)
