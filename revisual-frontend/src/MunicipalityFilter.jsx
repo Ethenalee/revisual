6f370cad379f1a1254c7145bd99ad49ec76bce8d
ethenalee <yeonji900111@gmail.com>
2019-01-30 22:34:20 +0000

    graph working

--- revisual-frontend/src/MunicipalityFilter.jsx
+++ revisual-frontend/src/MunicipalityFilter.jsx
@@ -2,9 +2,6 @@ import React, {Component} from 'react';
 
  
 class MunicipalityFilter extends Component {
-  constructor() {
-    super();
-  }
 
   saleChange = (event) => {
     this.props.saleChange(event.target.value);
