92ad00c7a6ff891c589a3b98dc24a33d5e05bd33
ethenalee <yeonji900111@gmail.com>
2019-01-31 01:47:31 +0000

    label style added, number fixed, data building

--- revisual-frontend/src/MunicipalityBrief.jsx
+++ revisual-frontend/src/MunicipalityBrief.jsx
@@ -1,43 +1,16 @@
 import React, {Component} from 'react';
  // eslint-disable-next-line
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Bar } from "react-chartjs-2";
-import { MDBContainer } from "mdbreact";
+import MunicipalityBriefChart from './MunicipalityBriefChart';
+import MunicipalityBriefTable from './MunicipalityBriefTable';
 
 
 class MunicipalityBrief extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       data: null,
-      barChartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          xAxes: [
-              {
-                barPercentage: 1,
-                gridLines: {
-                  display: true,
-                  color: "rgba(0, 0, 0, 0.1)"
-                }
-              }
-            ],
-          yAxes: [
-            {
-              gridLines: {
-                display: true,
-                color: "rgba(0, 0, 0, 0.1)"
-              },
-              ticks: {
-                beginAtZero: true
-              }
-            }
-          ]
-        }
-      }
-    };
+    }
   }
 
   componentWillMount = () => {
@@ -57,40 +30,11 @@ class MunicipalityBrief extends Component {
 
   render() {
     let data = this.state.data;
-    let dataBar = {
-      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
-      datasets: [
-        {
-          label: "#1",
-          data: [12, 39, 3, 50, 2, 32, 84],
-          backgroundColor: "rgba(245, 74, 85, 0.5)",
-          borderWidth: 1
-        },
-        {
-          label: "#2",
-          data: [56, 24, 5, 16, 45, 24, 8],
-          backgroundColor: "rgba(90, 173, 246, 0.5)",
-          borderWidth: 1
-        },
-        {
-          label: "#3",
-          data: [12, 25, 54, 3, 15, 44, 3],
-          backgroundColor: "rgba(245, 192, 50, 0.5)",
-          borderWidth: 1
-        }
-      ]
-    };
     return (
       <div className="brief">
-        This is Market trend space
-        {this.props.municipality}, {this.props.areacode}, {this.props.duration}, {this.props.sale_lease}
-        {data && data.average_sold_price}
-        <div className="brief-chart">
-          <MDBContainer>
-            <h3 className="mt-5">Radar chart</h3>
-            <Bar data={dataBar} options={this.state.barChartOptions} />
-          </MDBContainer>
-        </div>
+        <div className="market-trends">{data && data.municipality.municipality} Market Trends</div>
+        <MunicipalityBriefTable data={this.state.data}/>
+        <MunicipalityBriefChart data={this.state.data}/>
         <Link className="go-to-detail"  to ={{
           pathname: `/municipalities/${this.props.areacode}`, 
           state: { 
