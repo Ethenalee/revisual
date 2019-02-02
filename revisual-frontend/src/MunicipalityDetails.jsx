6631ba09d71c3cb1f2a5f1478107f92046b64e83
Zixia Lu <zixialu@gmail.com>
2019-02-02 14:34:56 -0500

    Prettier municipality details

--- revisual-frontend/src/MunicipalityDetails.jsx
+++ revisual-frontend/src/MunicipalityDetails.jsx
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
 import MunicipalityDetailsHead from './MunicipalityDetailsHead';
 import MunicipalityReport from './MunicipalityReport';
 import MunicipalityDetailsReportSummary from './MunicipalityDetailsReportSummary';
 import MunicipalityDetailsFooter from './MunicipalityDetailsFooter';
 import MunicipalityReportHead from './MunicipalityReportHead';
-
 
 //Report Details page general content
 class MunicipalityDetails extends Component {
@@ -16,22 +16,30 @@ class MunicipalityDetails extends Component {
     const data = this.props.location.state.data;
     const sale_lease = this.props.location.state.sale_lease;
     const duration = this.props.location.state.duration;
-    
+
     return (
       <section className="third-page">
-          <MunicipalityDetailsHead/>
-          <div className="details page">
-            <div className="reportHead">
-            <MunicipalityReportHead data = {data} sale_lease={sale_lease} duration={duration}/>
-            </div>
-            <MunicipalityReport data = {data} sale_lease={sale_lease}/>
-            <div className="reportSummary">
-              <MunicipalityDetailsReportSummary/>
-            </div>
-            <div className="reportFooter">
-              <MunicipalityDetailsFooter/>
-            </div>
+        <MunicipalityDetailsHead />
+
+        <div className="details page">
+          <div className="reportHead">
+            <MunicipalityReportHead
+              data={data}
+              sale_lease={sale_lease}
+              duration={duration}
+            />
           </div>
+
+          <MunicipalityReport data={data} sale_lease={sale_lease} />
+
+          <div className="reportSummary">
+            <MunicipalityDetailsReportSummary />
+          </div>
+
+          <div className="reportFooter">
+            <MunicipalityDetailsFooter />
+          </div>
+        </div>
       </section>
     );
   }
