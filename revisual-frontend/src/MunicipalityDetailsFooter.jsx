30a778695fbcdf0d7b0e2c8354f96ed3adc5fea5
ethenalee <yeonji900111@gmail.com>
2019-02-03 04:30:08 +0000

    text overlay bug fixed

--- revisual-frontend/src/MunicipalityDetailsFooter.jsx
+++ revisual-frontend/src/MunicipalityDetailsFooter.jsx
@@ -31,14 +31,14 @@ class MunicipalityDetailsFooter extends Component {
         <div className="report-footer">
           <div className="report-logo"> <img alt="logo" src={this.state.data ? this.state.data.logo_url : ''} height="48" width="48"/> </div>
           <div className="report-contact">
-            <div>
-              <span className="contact-labels">Cell: </span><span>{user ? user.phone_cell : ''}</span>&nbsp;&nbsp;<span className="contact-labels">Office: </span><span>{user ? user.phone_office : ''}</span>&nbsp;&nbsp;<span className="contact-labels">Fax: </span><span>{user ? user.phone_fax : ''}</span> 
+            <div className="report-conact-text">
+              <span className="contact-labels">Cell:</span>&nbsp;<span>{user ? user.phone_cell : ''}</span>&nbsp;&nbsp;<span className="contact-labels">Office: </span>&nbsp;<span>{user ? user.phone_office : ''}</span>&nbsp;&nbsp;<span className="contact-labels">Fax: </span>&nbsp;<span>{user ? user.phone_fax : ''}</span> 
             </div>
-            <div>
-              <span className="contact-labels">Email: </span><span>{user ? user.email : ''}</span>&nbsp;&nbsp;<span className="contact-labels">Website: </span><span>{user ? user.website_url : ''}</span>
+            <div className="report-conact-text">
+              <span className="contact-labels">Email:</span>&nbsp;<span>{user ? user.email : ''}</span>&nbsp;&nbsp;<span className="contact-labels">Website: </span>&nbsp;<span>{user ? user.website_url : ''}</span>
             </div>
-            <div>
-              <span className="contact-labels">Address: </span><span>{user ? user.address_1 : ''} {user ? user.address_2 : ''} {user ? user.city : ''} </span>
+            <div className="report-conact-text">
+              <span className="contact-labels">Address:</span>&nbsp;<span>{user ? user.address_1 : ''} {user ? user.address_2 : ''} {user ? user.city : ''} </span>
             </div>
           </div>
         </div>
